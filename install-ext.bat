391d6b1ee3a178e54f9563b3c7556b7d6cf68fc4
Vien Dinh <maxvien@gmail.com>
2018-06-26 10:39:09 +0700

    U

---
+++ install-ext.bat
@@ -0,0 +1,5 @@
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension shinnn.stylelint
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension xabikos.JavaScriptSnippets
