cb25147b5d0802838f7ab18c6b29f1680e3dee7c
Vien Dinh <maxvien@gmail.com>
2018-07-05 12:48:50 +0700

    Fix app-carousel

--- src/blocks/app-carousel/app-carousel.js
+++ src/blocks/app-carousel/app-carousel.js
@@ -3,6 +3,8 @@ var appCarousel = this;
 $(appCarousel).owlCarousel({
   items: 1,
   nav: true,
+  autoplay: true,
+  loop: true,
   onInitialized: function () {
     // .app-carousel__item
     $('.app-carousel__item').each(function () {
@@ -11,15 +13,11 @@ $(appCarousel).owlCarousel({
 
     // .app-carousel__nav
     $(appCarousel).find('.owl-nav').addClass('app-carousel__nav');
-    $(appCarousel).find('.owl-nav').width($(appCarousel).height());
+    // $(appCarousel).find('.owl-nav').width($(appCarousel).height());
     $(appCarousel).find('.owl-nav .owl-prev').addClass('app-carousel__nav-prev');
     $(appCarousel).find('.owl-nav .owl-prev').html('<i class="fas fa-long-arrow-alt-left"></i>');
     $(appCarousel).find('.owl-nav .owl-next').addClass('app-carousel__nav-next');
     $(appCarousel).find('.owl-nav .owl-next').html('<i class="fas fa-long-arrow-alt-right"></i>');
     $('<span class="app-carousel__nav-text">Featured Article</span>').insertAfter('.app-carousel__nav-prev');
-
-    // .app-carousel__dots
-    $(appCarousel).find('.owl-dots').addClass('app-carousel__dots');
-    $(appCarousel).find('.owl-dots .owl-dot').addClass('app-carousel__dots-dot');
   }
 });
